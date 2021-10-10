f32315c8fe009a7525ac83a67d37c9fc4678eae3
Adriano de Jesus <adrianoscript@gmail.com>
2021-10-10 13:46:52 -0300

    projeto01 variavel.py

---
+++ Introdução a Linguagem Python/Variavel.py
@@ -0,0 +1,35 @@
+"""
+Python suporta dois tipos números - inteiros e números de ponto flutuante.
+"""
+
+# Prara definir um número inteiro, use a seguinte sintaxe:
+inteiro = 7
+print(inteiro)
+
+# Prara definir um número de ponto flutuante, use a seguinte notação:
+ponto_flutuante = 7.5
+print(ponto_flutuante)
+
+# Obs: Um inteiro não possui casa decimal, diferente do ponto flutuante.
+
+
+"""
+Outro tipo de variável é a string, pode ser definida como uma sequencia de caracteres.
+"""
+# As strings são definidas com aspas duplas:
+string = 'hello'
+print(string)
+
+
+# Obs:
+# Operadores matemáticos simples podem ser executados em números e strings:
+um = 1
+dois = 2
+tres = um + dois
+print(tres)
+
+
+hello = 'hello'
+world = 'world'
+helloWorld = hello + " " + world
+print(helloWorld)
